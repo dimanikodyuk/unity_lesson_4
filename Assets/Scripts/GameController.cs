f18a228f2b1a0013e605797ab57f5c16a3a7b293
dimanikodyuk <dimanikodyuk@gmail.com>
2022-05-15 15:41:52 +0300

    Update project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        GameMenu._onButtonCredits += ButtonCredits;
+        GameMenu._onButtonCinematic += ButtonCinematics;
+        GameMenu._onButtonExit += ButtonExit;
+        GameMenu._onButtonApply += ButtonApply;
+        GameMenu._onResolutionDropdown += ResolutionDropdown;
+        GameMenu._onQualitynDropdown += QualityDropdown;
+        GameMenu._onMasterVolume += MasterVolume;
+        GameMenu._onMusicVolume += MusicVolume;
+        GameMenu._onFullscreenToggle += FullscreenToggle;
+        GameMenu._onSoundBackgroundToggle += SoundBackgroundToggle;
+        GameMenu._onChallengingToggle += ChallengingToggle;
+        GameMenu._onFriendsToggle += FriendsToggle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ButtonCredits()
+    {
+        Debug.Log("Нажата кнопка [ButtonCredits]");
+    } 
+    
+    
+    private void ButtonCinematics()
+    {
+        Debug.Log("Нажата кнопка [ButtonCinematics]");
+    } 
+    
+    private void ButtonExit()
+    {
+        Debug.Log("Нажата кнопка [ButtonExit]");
+    }
+    
+    private void ButtonApply()
+    {
+        Debug.Log("Нажата кнопка [ButtonApply]");
+    }  
+
+    private void ResolutionDropdown(int indexValue, string valueText)
+    {
+        Debug.Log($"Значение выпадающего списка [ResolutionDropdown] изменено на индекс {indexValue}, что равно {valueText}");
+    }  
+
+    private void QualityDropdown(int indexValue, string valueText)
+    {
+        Debug.Log($"Значение выпадающего списка [ResolutionDropdown] изменено на индекс {indexValue}, что равно {valueText}");
+    }
+
+    private void MasterVolume(float volumeValue)
+    {
+        Debug.Log($"Изменено значение параметра MasterVolume на {volumeValue}");
+    }
+
+    private void MusicVolume(float volumeValue)
+    {
+        Debug.Log($"Изменено значение параметра MusicVolume на {volumeValue}");
+    }
+
+    private void FullscreenToggle(bool isFullscreen)
+    {
+        Debug.Log($"[FullscreenToggle] Изменено значение на {isFullscreen}");
+    }
+
+    private void SoundBackgroundToggle(bool isSoundBackground)
+    {
+        Debug.Log($"[SoundBackgroundToggle] Изменено значение на {isSoundBackground}");
+    }
+
+    private void ChallengingToggle(bool isChallenging)
+    {
+        Debug.Log($"[ChallengingToggle] Изменено значение на {isChallenging}");
+    }
+    
+    private void FriendsToggle(bool isFriends)
+    {
+        Debug.Log($"[FriendsToggle] Изменено значение на {isFriends}");
+    }
+}
---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour
+{
+    [SerializeField] Button _buttonCredits;
+    [SerializeField] Button _buttonCinematic;
+    [SerializeField] Button _buttonExit;
+    [SerializeField] Button _buttonApply;
+    [SerializeField] TMP_Dropdown  _resolutionDropdown;
+    [SerializeField] TMP_Dropdown _qualityDropdown;
+    [SerializeField] Slider _masterVolumeSlider;
+    [SerializeField] Slider _musicVolumeSlider;
+    [SerializeField] Toggle _fullscreenToggle;
+    [SerializeField] Toggle _soundBackgroundToggle;
+    [SerializeField] Toggle _challengingToggle;
+    [SerializeField] Toggle _friendsSpectateToggle;
+
+    public static Action _onButtonCredits;
+    public static Action _onButtonCinematic;
+    public static Action _onButtonExit;
+    public static Action _onButtonApply;
+    public static Action<int, string> _onResolutionDropdown;
+    public static Action<int, string> _onQualitynDropdown;
+    public static Action<float> _onMasterVolume;
+    public static Action<float> _onMusicVolume;
+    public static Action<bool> _onFullscreenToggle;
+    public static Action<bool> _onSoundBackgroundToggle;
+    public static Action<bool> _onChallengingToggle;
+    public static Action<bool> _onFriendsToggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _buttonCredits.onClick.AddListener(ButtonCredits);
+        _buttonCinematic.onClick.AddListener(ButtonCinematic);
+        _buttonExit.onClick.AddListener(ButtonExit);
+        _buttonApply.onClick.AddListener(ButtonApply);
+        _resolutionDropdown.onValueChanged.AddListener(ResolutionDropdown);
+        _qualityDropdown.onValueChanged.AddListener(QualityDropdown);
+        _masterVolumeSlider.onValueChanged.AddListener(MasterVolume);
+        _musicVolumeSlider.onValueChanged.AddListener(MusicVolume);
+        _fullscreenToggle.onValueChanged.AddListener(FullscreenToggle);
+        _soundBackgroundToggle.onValueChanged.AddListener(SoundBackgroundToggle);
+        _challengingToggle.onValueChanged.AddListener(ChallengingToggle);
+        _friendsSpectateToggle.onValueChanged.AddListener(FriendsToggle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ButtonCredits()
+    {
+        _onButtonCredits?.Invoke();
+    }
+
+    private void ButtonCinematic()
+    {
+        _onButtonCinematic?.Invoke();
+    }
+
+    private void ButtonExit()
+    {
+        _onButtonExit?.Invoke();
+    }
+
+    private void ButtonApply()
+    {
+        _onButtonApply?.Invoke();
+    }
+
+    private void ResolutionDropdown(int value)
+    {
+        _onResolutionDropdown?.Invoke(value, _resolutionDropdown.options[value].text);
+    } 
+    
+    private void QualityDropdown(int value)
+    {
+        _onQualitynDropdown?.Invoke(value, _qualityDropdown.options[value].text);
+    }
+
+    private void MasterVolume(float volumeValue)
+    {
+        _onMasterVolume?.Invoke(volumeValue);
+    }
+    
+    private void MusicVolume(float volumeValue)
+    {
+        _onMusicVolume?.Invoke(volumeValue);
+    }
+    
+    private void FullscreenToggle(bool isFullscreen)
+    {
+        _onFullscreenToggle?.Invoke(isFullscreen);
+    } 
+    
+    private void SoundBackgroundToggle(bool isSoundBackground)
+    {
+        _onSoundBackgroundToggle?.Invoke(isSoundBackground);
+    } 
+    
+    private void ChallengingToggle(bool isChallenging)
+    {
+        _onChallengingToggle?.Invoke(isChallenging);
+    }
+    
+    private void FriendsToggle(bool isFriends)
+    {
+        _onChallengingToggle?.Invoke(isFriends);
+    }
+}
